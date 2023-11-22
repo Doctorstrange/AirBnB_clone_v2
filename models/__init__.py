da096b6e353913a46a66d5a64dd9a74d6296a479
Doctorstrange <Doctorstrange>
2023-11-22 16:00:21 -0600

    Add a new public instance method: def delete(self, obj=None): to delete obj

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""nstantiates an object of class FileStorage"""
+import os
+
+
+main_storage = os.getenv('HBNB_TYPE_STORAGE')
+
+
+if main_storage == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()#!/usr/bin/python3
+"""nstantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
 
