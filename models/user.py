088a811908ee223dca60f2f6140b28100d9bb0b6
Doctorstrange <Doctorstrange>
2023-11-22 21:05:58 -0600

    chat

--- models/user.py
+++ models/user.py
@@ -15,4 +15,3 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
