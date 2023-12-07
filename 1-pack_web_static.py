b1d89633ca8902a9975cd51143fad989593b8b85
Doctorstrange <Doctorstrange>
2023-12-07 17:44:19 -0600

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,7 +13,7 @@ def do_pack():
     time = datetime.now()
     archive_name = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('sudo mkdir -p versions')
-    archive = local('tar -cvzf versions/{} web_static'.format(archive))
+    archive = local('tar -cvzf versions/{} web_static'.format(archive_name))
     if archive is not None:
         return archive_name
     else:
