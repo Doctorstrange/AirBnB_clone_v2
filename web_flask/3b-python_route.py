8a72486e564b5cca4a89a3b2eaaa8e3dae14a239
Doctorstrange <Doctorstrange>
2024-01-01 15:37:41 -0600

    3-python_route.py

--- web_flask/3b-python_route.py
+++ web_flask/3b-python_route.py
@@ -24,7 +24,6 @@ def cplace(text):
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def piithon(text='is cool'):
     """display “Python ”, followed by the value of the text variable """
@@ -32,4 +31,4 @@ def piithon(text='is cool'):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5055)
