5dfd2d30666d837ccdbd1a2c01dac2e68427feb2
doctorstrange <doctorstrange247@gmail.com>
2024-01-25 14:02:07 +0000

    /

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask("__name__")
 
 
-@app.route('/airbnb-onepage', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
     """web application must be listening on 0.0.0.0, port 5000"""
     return ("Hello HBNB!")
